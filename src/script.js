5de72a5a197715882e6396faad138f5c66d91317
Denver-Meadows <meadows8200@gmail.com>
2021-08-10 19:04:01 -0400

    Working without sunrise and sunset

--- src/script.js
+++ src/script.js
@@ -4,11 +4,6 @@ const userInput = document.querySelector('.location__selection__input');
 const userInputBtn = document.querySelector('.location__selection__button');
 const dateAndTime = document.querySelector('.weather__data__location__time');
 
-// Need to do
-// Take city or zip
-// Render Errors
-// Format sunrise and sunset
-
 const state = {
   data: [],
   timeZone: '',
@@ -46,6 +41,44 @@ const convertDate = function(data) {
   return `${month}/${day}/${year}`
 };
 
+const localTime = function(time) {
+  const offset = utcOffset();
+  console.log(offset)
+  const [hour, minutes] = time.split(':')
+  console.log(hour)
+  const formattedHour = getFormattedHour(hour) - offset
+
+  return `${formattedHour >= 13 ? formattedHour - 12 : formattedHour}:${minutes} ${formattedHour < 12 ? 'am' : 'pm'}`
+}
+
+const renderWeatherData = async function(data) {
+
+  const html = `
+    <div class="weather__data__location">
+      <p class="weather__data__location__city">Weather Today <br>${data.city_name}, ${data.state_code}</p>
+      <p class="weather__data__location__time">${convertDate(data.ob_time)}</p>
+    </div>
+    <div class="weather__data__current">
+      <p class="weather__data__current__icon"><img src="src/icons/${data.weather.icon}.png" alt=""></p>
+      <p class="weather__data__current__description">${data.weather.description}</p>
+    </div>
+    <div class="weather__data__temp">
+      <p class="weather__data__temp__actual">${Math.round(data.temp)}°</p>
+      <p class="weather__data__temp__feels-like">Feels like <span class="number">${Math.round(data.app_temp)}°</span></p>
+    </div>
+    <div class="weather__data__additional-info">
+      <p class="weather__data__additional-info__humidity">Humidity:   <span class="number">${data.rh}%</span></p>
+      <p class="weather__data__additional-info__wind">Wind:  <span class="number">${data.wind_spd}</span>mph ${data.wind_cdir}</p>
+      <p class="weather__data__additional-info__air-quality">Air Quality:  <span class="number">${data.aqi}</span></p>
+      <p class="weather__data__additional-info__dewpt">Dew Point:   <span class="number">${data.dewpt}°</span></p>
+    </div>
+  `
+  weatherDataContainer.insertAdjacentHTML('beforeend', html)
+}
+
+/*
+
+
 
 // works for local time
 const getFormattedHour = function(hour) {
@@ -63,46 +96,8 @@ const utcOffset = function() {
   if (state.timeZone === "America/Honolulu") return 10;
 }
 
-const localTime = function(time) {
-  const offset = utcOffset();
-  console.log(offset)
-  const [hour, minutes] = time.split(':')
-  console.log(hour)
-  const formattedHour = getFormattedHour(hour) - offset
-
-  return `${formattedHour >= 13 ? formattedHour - 12 : formattedHour}:${minutes} ${formattedHour < 12 ? 'am' : 'pm'}`
-}
-
-
-
-
-
-
-const renderWeatherData = async function(data) {
-
-  const html = `
-    <div class="weather__data__location">
-      <p class="weather__data__location__city">Weather Today <br>${data.city_name}, ${data.state_code}</p>
-      <p class="weather__data__location__time">${convertDate(data.ob_time)}</p>
-    </div>
-    <div class="weather__data__current">
-      <p class="weather__data__current__icon"><img src="src/icons/${data.weather.icon}.png" alt=""></p>
-      <p class="weather__data__current__description">${data.weather.description}</p>
-    </div>
-    <div class="weather__data__temp">
-      <p class="weather__data__temp__actual">${data.temp}°</p>
-      <p class="weather__data__temp__feels-like">Feels like <span class="number">${Math.round(data.app_temp)}°</span></p>
-    </div>
-    <div class="weather__data__additional-info">
-      <p class="weather__data__additional-info__humidity">Humidity:   <span class="number">${data.rh}%</span></p>
-      <p class="weather__data__additional-info__wind">Wind:  <span class="number">${data.wind_spd}</span>mph ${data.wind_cdir}</p>
-      <p class="weather__data__additional-info__air-quality">Air Quality:  <span class="number">${data.aqi}</span></p>
-      <p class="weather__data__additional-info__dewpt">Dew Point:   <span class="number">${data.dewpt}°</span></p>
-    </div>
     <div class="weather__data__sun">
       <p class="weather__data__sun-rise">Sunrise:   ${localTime(data.sunrise)}</p>
       <p class="weather__data__sun-set">Sunset:   ${localTime(data.sunset)}</p>
     </div>
-  `
-  weatherDataContainer.insertAdjacentHTML('beforeend', html)
-}
+*/
